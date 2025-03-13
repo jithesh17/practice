3c03e7009efcb944cd0461d5e6b83fd3ca5e0469
jithesh17 <jitheshraju123@gmail.com>
2025-03-13 18:34:39 +0530

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,14 +1,18 @@
-public class Palindrome{
-public static void main(String[] args) {
+public class Palindrome
+{
+public static void main(String[] args)
+{
 
 int numbers[] = new int[]{121,13,34,11,22,54};
 
-for(int i=0; i < numbers.length; i++){
+for(int i=0; i < numbers.length; i++)
+{
 int number = numbers[i];
 int reversedNumber = 0;
 int temp=0;
 
-while(number > 0){
+while(number > 0)
+{
 temp = number % 10;
 number = number / 10;
 reversedNumber = reversedNumber * 10 + temp;
